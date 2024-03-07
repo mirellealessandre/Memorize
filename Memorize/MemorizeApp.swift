8701a0606151fcb13e3ea5683bfbae687d4adfc5
Mirelle Alessandre <mirelle.alessandre@arctouch.com>
2024-03-07 15:21:50 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Mirelle Alessandre on 07/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
