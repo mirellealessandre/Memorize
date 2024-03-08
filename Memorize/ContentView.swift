17e582a320702479d7f95a803667cfec8664b01b
Mirelle Alessandre <mirelle.alessandre@arctouch.com>
2024-03-08 14:11:13 -0300

    Refactor ContentView and create cardCountAdjuster function

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -5,17 +5,55 @@ import SwiftUI
 }
 
 struct ContentView: View {
-    let emojis: [String] = ["🐮", "🪰", "🐗", "🐷", "🐙"]
+    let emojis: [String] = ["🐮", "🪰", "🐗", "🐷", "🐙", "🐥", "🦄", "🐔", "🦇", "🐞", "🪼", "🦧"]
+    
+    @State var cardCount: Int = 4
     
     var body: some View {
+        VStack {
+            cards
+            cardCountAdjusters
+        }
+        .padding()
+    }
+    
+    var cardCountAdjusters: some View {
         HStack {
-            ForEach(emojis.indices, id: \.self) { index in
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    var cards: some View {
+        HStack {
+            ForEach(0..<cardCount, id: \.self) { index in
                 CardView(content: emojis[index])
             }
         }
-        .foregroundColor(.orange)
         .padding()
+        .foregroundColor(.orange)
     }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardRemover: some View {
+        return cardCountAdjuster(by: -1, symbol: "rectangle.stack.fill.badge.minus")
+    }
+    
+    var cardAdder: some View {
+        return cardCountAdjuster(by: +1, symbol: "rectangle.stack.fill.badge.plus")
+    }
+    
 }
 
 struct CardView: View {
