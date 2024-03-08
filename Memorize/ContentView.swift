b3f1ed24e54e501c11cae2b0643e99f178ac233d
Mirelle Alessandre <mirelle.alessandre@arctouch.com>
2024-03-08 14:30:25 -0300

    Refactor cards

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             cards
+            Spacer()
             cardCountAdjusters
         }
         .padding()
@@ -28,7 +29,7 @@ struct ContentView: View {
     }
     
     var cards: some View {
-        HStack {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
             ForEach(0..<cardCount, id: \.self) { index in
                 CardView(content: emojis[index])
             }
